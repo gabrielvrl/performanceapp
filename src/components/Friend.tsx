d9d0790d529572a29d9a596667c5fe7c87f3a9ec
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2023-02-18 12:13:16 -0300

    memo

--- src/components/Friend.tsx
+++ src/components/Friend.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { Text } from 'react-native';
 
 interface FriendProps {
   data: {
+    id: number;
     name: string;
     likes: number;
   }
 }
 
-export const Friend = ({ data }: FriendProps) => {
+const FriendComponent = ({ data }: FriendProps) => {
   return (
     <Text>
       {data.name} - Likes: {data.likes}
     </Text>
   );
 }
+
+export const Friend = memo(FriendComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.data, nextProps.data);
+});
